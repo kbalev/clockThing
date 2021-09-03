202ff451088a1d1053e6e4f5bb94958e5e10908d
kbalev <kamen_balev@yahoo.com>
2021-09-03 15:34:28 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Clock } from './Clock';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function tick() {
+    ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );
+}
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
